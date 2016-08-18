24ed4c27dc747851165488b6b4c1d9368b611d11
BaconBao <lernhern@gmail.com>
2016-08-19 03:05:47 +0800

    Chrome Extension v2.0.0
    v2.0.0 (2016/08/16)
    - TaiwaneseWizard 2.0 !
    - More efficient: Only speak Chinese.
    - More useful: Select to search dictionary and listen the voice.
    - More easy: Easy to known how to use, and easy to share.

--- chrome/core.js
+++ chrome/core.js
@@ -1,8 +1,30 @@
+localStorage["version"] = chrome.runtime.getManifest().version;
+
+String.prototype.replaceAt = function(index, character) {
+    return this.substr(0, index) + character + this.substr(index+character.length);
+}
+
+function numberChange(text) {
+	for(var i=0; i<text.length; i++){
+		if(text[i]=='０') text = text.replaceAt(i, '0');
+		if(text[i]=='１') text = text.replaceAt(i, '1');
+		if(text[i]=='２') text = text.replaceAt(i, '2');
+		if(text[i]=='３') text = text.replaceAt(i, '3');
+		if(text[i]=='４') text = text.replaceAt(i, '4');
+		if(text[i]=='５') text = text.replaceAt(i, '5');
+		if(text[i]=='６') text = text.replaceAt(i, '6');
+		if(text[i]=='７') text = text.replaceAt(i, '7');
+		if(text[i]=='８') text = text.replaceAt(i, '8');
+		if(text[i]=='９') text = text.replaceAt(i, '9');
+	}
+	return text;
+}
 function openPage(text, tab) {
 	if(text&&text.length>0) {
 		var pageTab={ url:chrome.extension.getURL('page.html')};
 	}
 	if(tab) pageTab.index = tab.index+1;
+	text = numberChange(text);
 	if(text=='0') text='０';
 	localStorage["tmpText"] = text;
 	chrome.tabs.create(pageTab);
@@ -15,5 +37,3 @@ chrome.contextMenus.create({
 		openPage(info.selectionText, tab);
 	}
 });
-
-
--- chrome/page.js
+++ chrome/page.js
@@ -1,3 +1,7 @@
+String.prototype.replaceAt = function(index, character) {
+    return this.substr(0, index) + character + this.substr(index+character.length);
+}
+
 function set2default(){
 	localStorage["setting-speaker"] = 'TW_LIT_AKoan';
 	localStorage["setting-volume"] = 100;
@@ -10,6 +14,102 @@ function faceInitFunc(){
 	setTimeout(function(){$('#face').text('> _ <');},10);
 	setTimeout(function(){$('#face').text('^ _ ^');},800);
 }
+
+function textReplce_forSpeak(text){
+	var pa = [',', ':', ';', '?', '!', '。', '，', '：', '；', '？', '！', '、', '　'];
+	var output = '';
+	text = text.trim(text);
+	text = text.replace(/\ \s+/g, ' ');
+	for(var i=0; i<text.length; i++){
+		if( text[i]==' ' && ifChinese(text, i-1) && ifChinese(text, i+1) ) output = output + '　';
+		if( ifChinese(text, i) ) output = output + text[i];
+		if( jQuery.inArray(text[i], pa)>=0 ) output = output + text[i];
+		if( text[i].match(/\d+/g) ) output = output + text[i];
+	}
+	output = output.replace(/\s+/g, '');
+	return output;
+}
+
+function textReplce_forEdit(text){
+	var pa = [',', ':', ';', '?', '!', '。', '，', '：', '；', '？', '！', '、', '　'];
+	var output = '';
+	text = text.trim(text);
+	text = text.replace(/\ \s+/g, ' ');
+	for(var i=0; i<text.length; i++){
+		if( text[i]==' ' && ifChinese(text, i-1) && ifChinese(text, i+1) ){
+			text = text.replaceAt(i, '　');
+		}
+		output = output + text[i];			
+	}
+	output = output.replace(/\<span class\=\"speak\_n\"\>\s+\<\/span\>/g, '');
+	return output;
+}
+
+function textReplce_forHTML(text){
+	var pa = [',', ':', ';', '?', '!', '。', '，', '：', '；', '？', '！', '、', '　'];
+	var output = '';
+	text = text.trim(text);
+	text = text.replace(/\ \s+/g, ' ');
+	for(var i=0; i<text.length; i++){
+		var ifSpeak = false;
+		if( text[i]==' ' && ifChinese(text, i-1) && ifChinese(text, i+1) ){
+			text = text.replaceAt(i, '　');
+		}
+		if( ifChinese(text, i) ) ifSpeak = true;
+		if( jQuery.inArray(text[i], pa)>=0 ) ifSpeak = true;
+		if( text[i].match(/\d+/g) ) ifSpeak = true;
+		if(ifSpeak){
+			output = output + '<span class="speak_y">' + text[i] + '</span>';
+		}else{
+			output = output + '<span class="speak_n">' + text[i] + '</span>';
+		}
+	}
+	output = output.replace(/\<span class\=\"speak\_n\"\>\s+\<\/span\>/g, '');
+	return output;
+}
+
+function textReplce_onlyChinese(text){
+	var output = '';
+	for(var i=0; i<text.length; i++){
+		if( ifChinese(text, i) ) output = output + text[i];
+	}
+	return output;
+}
+
+function ifChinese(text, point){
+	if( text.charCodeAt(point)>=0x4E00 && text.charCodeAt(point)<=0x9FA5 ) {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function HowManyChinese(text){
+	var v = 0;
+	for(var i=0; i<text.length; i++){
+    	text.charCodeAt(i);
+		if( text.charCodeAt(i)>=0x4E00 && text.charCodeAt(i)<=0x9FA5 ) v++;
+	}
+	return v;
+}
+
+function getSelText(){
+	var text = '';
+	if (window.getSelection) {
+		text = window.getSelection();
+	} else if(document.getSelection) {
+		text = document.getSelection();
+	} else if(document.selection) {
+		text = document.selection.createRange().text;
+	}
+	return text;
+}
+
+function padLeft(str, len) {
+    str = '' + str;
+    return str.length >= len ? str : new Array(len - str.length + 1).join("0") + str;
+}
+
 
 function stratTTS_first(text){
 	if(localStorage["setting-volume"]==null){
@@ -70,12 +170,51 @@ function stratTTS_first(text){
 		},
 		error: function(){
 			$('#face').text('x _ x');
-			alert('無法連接，可能是您的網路不通或是本服務維護中。');
+			$('#nofi').hide();
+			alert('錯誤：無法連接，可能是您的網路不通或是本服務維護中。');
 		}
 	});
 }
 
+function stratDic(text){
+	var output = new Array();
+	output['dicName'] = text;
+	output['dicID'] = '';
+	output['dicRoma'] = '';
+	$.ajax({
+		url: 'http://twblg.dict.edu.tw/holodict_new/result.jsp?radiobutton=0&limit=20&querytarget=2&sample='+text+'&submit.x=0&submit.y=0',
+		type: 'get',
+		dataType: 'html',
+		async: false,
+		success: function(data) {
+			if( $(data).find('.all_space1').length ) {
+				var row = $(data).find('.all_space1');
+				output['dicRoma'] = $(row.find('td').get(2)).text();
+				if( $(row.find('td').get(1)).find('a').length ) {
+					var regExp = /n_no=(.*?)&/;
+					var matches = regExp.exec($(row.find('td').get(1)).find('a').attr('href'));
+					output['dicID'] = matches[0].replace('n_no=','');
+					output['dicID'] = output['dicID'].replace('&','');
+					output['dicID'] = padLeft(output['dicID'], 5);
+					output['dicID'] = 'http://twblg.dict.edu.tw/holodict_new/audio/'+output['dicID']+'.mp3';
+				}
+			}else{
+				output['dicName'] = 'oh_error';
+			}
+		},
+		error: function() {
+			output['dicName'] = 'oh_error';
+		}
+	});
+	return [
+		output['dicName'],
+		output['dicRoma'],
+		output['dicID']
+	];
+}
+
 $(document).ready(function(){
+
 	/*--- Page Start ---*/
 	if(sessionStorage["mainText"]==null){
 		sessionStorage["mainText"] = localStorage["tmpText"];
@@ -83,7 +222,15 @@ $(document).ready(function(){
 	}
 	localStorage.removeItem("tmpText");
 	var text = sessionStorage["mainText"];
-	$('#text2say').html(text);
+	var text_forHTML = textReplce_forHTML(text);
+	var text_forEdit = textReplce_forEdit(text);
+	var text_forSpeak = textReplce_forSpeak(text);
+
+	/*-- Init UI --*/
+	$('#icon_list').hide();
+	$('#version').text(localStorage["version"]);
+	$('#text2say').html( text_forHTML );
+	$('#text2edit>textarea').val( text_forEdit );
 
 	/*--- Title Change ---*/
 	if(text.length > 10){
@@ -91,9 +238,15 @@ $(document).ready(function(){
 	}else{
 		$('title').text('台語兒：「'+text+'」');
 	}
+
+	/*--- Check all text are Chinese --*/
+	if( HowManyChinese(text)<=0 ){
+		alert('錯誤：您似乎沒有選取到中文字串，請重新檢查。');
+		return false;
+	}
 	
 	/*--- Play voice --*/
-	stratTTS_first(text);
+	stratTTS_first( text_forSpeak );
 	
 	/*--- Logo Follow Mouse ---*/
 	var logoPos = $("#logo>img").position();
@@ -118,16 +271,90 @@ $(document).ready(function(){
 
 	/*--- Audio API ---*/
 	$('#ttsplayer').on('playing', function() {
-	   $('#face').text('^ 0 ^');
+		$('#face').text('^ 0 ^');
 	});
 	$('#ttsplayer').on('ended', function() {
 		$('#face').text('^ _ ^');
-	    faceInit = setInterval(faceInitFunc, 5000);
+		faceInit = setInterval(faceInitFunc, 5000);
+	});
+	$('#dicplayer').on('playing', function() {
+		$('#playDic').text('播放中...');
+	});
+	$('#dicplayer').on('ended', function() {
+		$('#playDic').text('聆聽讀音');
 	});
 
-	/*--- Get App Version --*/
-	$.getJSON('manifest.json').done(function(response) {
-	    $('#version').text(response.version);
-	});	
+	/*--- Select text to known ---*/
+	$("#text2say").mouseup(function(){
+		var tx = getSelText().toString();
+		if(tx!==''){
+			var dicResult = stratDic(tx);
+			if(dicResult['0']!=='oh_error'){
+				$('#select2known').removeClass('opac');
+				$("#select2known").text('【'+dicResult['0']+'】'+dicResult['1']);
+				if(dicResult['2']!=='') {
+					$('#dicplayer').get(0).setAttribute('src', dicResult['2']);
+					$("#select2known").append(' <span id="playDic">聆聽讀音</span>');
+				}
+				$('#playDic').click(function(){
+					$('#dicplayer').get(0).play();
+				});
+			}else{
+				$('#select2known').addClass('opac');
+				$("#select2known").text('【無結果】辭典中找不到符合的結果。');
+			}		
+		}
+	});
+
+	/*--- User click to fix text ---*/
+	$('#click2fix').click(function(){
+		var userText = textReplce_forEdit($('#text2edit>textarea').val());
+		var origText = textReplce_forEdit(sessionStorage["mainText"]);
+		if( userText==origText ){
+			$('#text2say').show();
+			$('#select2known').show();
+			$('#text2edit').hide();
+			return false;	
+		}
+		if( userText=='' || HowManyChinese(userText)<=0 ){
+			alert('錯誤：您的修正似乎未包含任何中文字。');
+		}else{
+			sessionStorage["mainText"] = textReplce_forEdit($('#text2edit>textarea').val());
+			window.location.reload();
+		}
+	});
+
+	/*--- Handle the hover event with non-support text ---*/ 
+	$('.speak_n').hover(function(){
+		$('.speak_n').css('background-color', '#eee');
+	}, function() {
+		$('.speak_n').css('background-color', '');
+	});
+
+	/*--- Tell user to click logo to replay ---*/ 
+	$('#logo').hover(function(){
+		$('#nofi').css('visibility', 'visible');
+	}, function() {
+		$('#nofi').css('visibility', 'hidden');
+	});
+
+	/*--- Click to show eidt box ---*/
+	$('.speak_n').click(function(){
+		$('#text2say').hide();
+		$('#select2known').hide();
+		$('#text2edit').show();
+	});
+
+	/*--- Share box ---*/
+	$('#icon_hover>span').hover(function(){
+		$('#icon_hover').fadeOut(function(){
+			$('#icon_list').fadeIn();
+		});
+	});
+	$('.share_cancel').click(function(){
+		$('#icon_hover').show();
+		$('#icon_list').hide();
+	});
+
 });
 
